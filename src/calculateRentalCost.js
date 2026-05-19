dd6462144ab4001d800b1e223571e6c738863318
Ivan Khyzhniak <hiznyak2014@gmail.com>
2026-05-19 02:39:47 +0200

    Fix unreachable code in calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,23 @@
  *
  * @return {number}
  */
+
+const PRICE_PER_DAY = 40;
+const SHORT_TERM_DISCOUNT = 20;
+const LONG_TERM_DISCOUNT = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days < 3) {
+    return days * PRICE_PER_DAY;
+  }
+
+  if (days >= 3 && days <= 6) {
+    return days * PRICE_PER_DAY - SHORT_TERM_DISCOUNT;
+  }
+
+  if (days >= 7) {
+    return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
+  }
 }
 
 module.exports = calculateRentalCost;
